f1fb20d86b046cfd7fcd78ed84e9cc50531e490d
Yegor Androsov <yegor.androsov@gmail.com>
2023-03-06 08:49:46 +0100

    add repositories

---
+++ Androsov.DataAccess/Interfaces/IRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Androsov.DataAccess.Interfaces
+{
+    public interface IMessageRepository
+    {
+        public Task<string?> Get(string username);
+        public Task Set(string username, string message);
+    }
+}
---
+++ Androsov.DataAccess/Repositories/MessageRepository.cs
@@ -0,0 +1,49 @@
+﻿using Androsov.DataAccess.Entities;
+using Androsov.DataAccess.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Androsov.DataAccess.Repositories
+{
+    public class MessageRepository : IMessageRepository
+    {
+        private readonly ApplicationDbContext dbContext;
+
+        public MessageRepository(ApplicationDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        public Task<string?> Get(string username)
+        {
+            return dbContext.Messages
+                .Where(x => x.User.UserName == username)
+                .Select(x => x.Text)
+                .FirstOrDefaultAsync();
+        }
+
+        public async Task Set(string username, string text)
+        {
+            var user = dbContext.Users.FirstOrDefault(x => x.UserName == username);
+
+            if (user == null)
+            {
+                throw new ArgumentException("Invalid username", nameof(username));
+            }
+
+            var message = new Message()
+            {
+                CreatedDate = DateTime.UtcNow,
+                Text = text,
+                UserId = user.Id,
+            };
+
+            dbContext.Messages.Add(message);
+            await dbContext.SaveChangesAsync();
+        }
+    }
+}
